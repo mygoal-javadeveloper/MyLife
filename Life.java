d0669a818316b2f70455e4352b9759a9c16e3cd2
mygoal-javadeveloper <mygoal.javasoftwaredeveloper@gmail.com>
2019-07-05 18:39:24 +0530

    Initial commit

---
+++ Life.java
@@ -0,0 +1,16 @@
+package nisrin_dhoondia_study;
+public class Life{
+public static void main(String[] args){
+Life obj1 = new Life();
+System.out.println(obj1.failure());
+}
+public String failure(){
+return (learnHarder());
+}
+public String learnHarder(){
+return (successForSure());
+}
+public String successForSure(){
+return "110% Success";
+}
+}
